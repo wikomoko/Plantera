2d2da76af364acbe5ba7bbeed9326cda008aa5bd
wikomoko <mokoindonesia45@gmail.com>
2021-08-05 14:35:24 +0700

    change main route to SplashScreen.kt using AndroidManifest.xml and change portrait rotation on activity_intro.xml and activity_splash_screen.xml

--- app/src/main/java/com/example/plantera/SplashScreen.kt
+++ app/src/main/java/com/example/plantera/SplashScreen.kt
@@ -1,11 +1,19 @@
 package com.example.plantera
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        //handler for auto move to another activity using second delay time
+        Handler().postDelayed({
+           val intent= Intent(this,IntroActivity::class.java)
+           startActivity(intent)
+        },2000)
     }
 }
