f3724b56b71ab94f01672dff2c3a2c689eb4d788
wikomoko <mokoindonesia45@gmail.com>
2021-08-05 14:51:52 +0700

    set data binding on IntroActivity.kt and find button also giving it intent function

--- app/src/main/java/com/example/plantera/IntroActivity.kt
+++ app/src/main/java/com/example/plantera/IntroActivity.kt
@@ -1,14 +1,36 @@
 package com.example.plantera
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.plantera.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    //variable for data biding feature
+    private lateinit var binding:ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        //display the xml
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_intro)
 
         //hidding splash screen
         supportActionBar?.hide()
+
+        //find button
+        val btn_started=findViewById<Button>(R.id.btn_started)
+
+        //button action
+        btn_started.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
