ae8af27a98d0eedd7273dfd2bbfb1564391368fe
wikomoko <mokoindonesia45@gmail.com>
2021-08-06 09:17:57 +0700

    build data for recycler view

---
+++ app/src/main/java/com/example/plantera/ProductData.kt
@@ -0,0 +1,70 @@
+package com.example.plantera
+
+class ProductData {
+
+    val image_of_products = intArrayOf(
+        R.drawable.carrot,
+        R.drawable.pok_choy,
+        R.drawable.paprica,
+        R.drawable.potato,
+        R.drawable.tomato,
+        R.drawable.cucumber,
+        R.drawable.pumkin,
+        R.drawable.radish,
+        R.drawable.chilli,
+        R.drawable.corn,
+        R.drawable.onion,
+    )
+
+    val name_of_products = arrayOf(
+        "Carrot",
+        "Pok Choy",
+        "Paprica",
+        "Potato",
+        "Tomato",
+        "Cucumber",
+        "Pumkin",
+        "Radish",
+        "Chilli",
+        "Corn",
+        "Onion"
+    )
+
+    val desc_of_products = arrayOf(
+        "The carrot (Daucus carota subsp. sativus) is a root vegetable, most commonly observed as orange in color, though purple, black, red, white, and yellow cultivars exist, all of which are domesticated forms of the wild carrot, Daucus carota, native to Europe and Southwestern Asia. The plant probably originated in Persia and was originally cultivated for its leaves and seeds. The most commonly eaten part of the plant is the taproot, although the stems and leaves are also eaten. The domestic carrot has been selectively bred for its greatly enlarged, more palatable, less woody-textured taproot.\n" +
+                "\n" +
+                "The carrot is a biennial plant in the umbellifer family, Apiaceae. At first, it grows a rosette of leaves while building up the enlarged taproot. Fast-growing cultivars mature within three months (90 days) of sowing the seed, while slower-maturing cultivars need a month longer (120 days). The roots contain high quantities of alpha- and beta-carotene, and are a good source of vitamin K and vitamin B6.\n" +
+                "\n" +
+                "The United Nations Food and Agriculture Organization (FAO) reports that world production of carrots and turnips (these plants are combined by the FAO) for 2018 was 40 million tonnes, with 45% of the world total grown in China. Carrots are widely used in many cuisines, especially in the preparation of salads, and carrot salads are a tradition in many regional cuisines.",
+
+        "pok choi (Brassica rapa subsp. chinensis) is a type of Chinese cabbage. Chinensis varieties do not form heads and have green leaf blades with lighter bulbous bottoms instead, forming a cluster reminiscent of mustard greens. Chinensis varieties are popular in southern China, East Asia, and Southeast Asia. Being winter-hardy, they are increasingly grown in Northern Europe. Now considered a subspecies of Brassica rapa, this group was originally classified as its own species under the name Brassica chinensis by Carl Linnaeus.[citation needed] They are a member of the family of Brassicaceae or Cruciferae, also commonly known as the mustards, the crucifers, or the cabbage family.\n",
+
+        "Paprica is a spice made from dried and ground red peppers.It is traditionally made from Capsicum annuum varietals in the Longum group, which also includes chili peppers, but the peppers used for paprika tend to be milder and have thinner flesh.In many languages, but not English, the word paprika also refers to the plant and the fruit from which the spice is made, as well as to peppers in the Grossum group (e.g. bell peppers).\n" +
+                "\n" +
+                "The peppers used in making paprika originate from North America, in particular Central Mexico, where they have been cultivated for centuries. The peppers were subsequently introduced to the Old World, when peppers were brought to Spain in the 16th century. The seasoning is used to add color and flavor to many types of dishes in diverse cuisines.\n" +
+                "\n" +
+                "The trade in paprika expanded from the Iberian Peninsula to Africa and Asia and ultimately reached Central Europe through the Balkans, which was then under Ottoman rule. This helps explain the Hungarian origin of the English term. In Spanish, paprika has been known as pimentón since the 16th century, when it became a typical ingredient in the cuisine of western Extremadura.Despite its presence in Central Europe since the beginning of Ottoman conquests, it did not become popular in Hungary until the late 19th century.\n" +
+                "\n" +
+                "Paprika can range from mild to hot – the flavor also varies from country to country – but almost all plants grown produce the sweet variety.Sweet paprika is mostly composed of the pericarp, with more than half of the seeds removed, whereas hot paprika contains some seeds, stalks, ovules, and calyces.The red, orange or yellow color of paprika is due to its content of carotenoids.",
+
+        "The potato is a root vegetable native to the Americas, a starchy tuber of the plant Solanum tuberosum, and the plant itself is a perennial in the nightshade family, Solanaceae.\n" +
+                "Wild potato species, originating in modern-day Peru, can be found throughout the Americas, from Canada to southern Chile.The potato was originally believed to have been domesticated by Native Americans independently in multiple locations,but later genetic testing of the wide variety of cultivars and wild species traced a single origin for potatoes, in the area of present-day southern Peru and extreme northwestern Bolivia. Potatoes were domesticated approximately 7,000–10,000 years ago there, from a species in the Solanum brevicaule complex.In the Andes region of South America, where the species is indigenous, some close relatives of the potato are cultivated.",
+
+        "The tomato is the edible berry of the plant Solanum lycopersicum, commonly known as a tomato plant. The species originated in western South America and Central America.The Nahuatl word tomatl gave rise to the Spanish word tomate, from which the English word tomato derived. Its domestication and use as a cultivated food may have originated with the indigenous peoples of Mexico.The Aztecs used tomatoes in their cooking at the time of the Spanish conquest of the Aztec Empire, and after the Spanish encountered the tomato for the first time after their contact with the Aztecs, they brought the plant to Europe. From there, the tomato was introduced to other parts of the European-colonized world during the 16th century.",
+
+        "Cucumber (Cucumis sativus) is a widely-cultivated creeping vine plant in the Cucurbitaceae gourd family that bears usually cylindrical fruits, which are used as vegetables.Considered an annual plant,there are three main varieties of cucumber — slicing, pickling, and burpless/seedless — within which several cultivars have been created. The cucumber originates from South Asia, but now grows on most continents, as many different types of cucumber are traded on the global market. In North America, the term wild cucumber refers to plants in the genera Echinocystis and Marah, though the two are not closely related.\n",
+
+        "A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin, and is most often deep yellow to orange in coloration.The thick shell contains the seeds and pulp. The name is most commonly used for cultivars of Cucurbita pepo, but some cultivars of Cucurbita maxima, C. argyrosperma, and C. moschata with similar appearance are also sometimes called \"pumpkin\". Native to North America (northeastern Mexico and the southern United States),pumpkins are one of the oldest domesticated plants, having been used as early as 7,500 to 5,000 BC.Pumpkins are widely grown for commercial use and as food, aesthetics, and recreational purposes. Pumpkin pie, for instance, is a traditional part of Thanksgiving meals in Canada and the United States, and pumpkins are frequently carved as jack-o'-lanterns for decoration around Halloween, although commercially canned pumpkin purée and pumpkin pie fillings are usually made from different kinds of winter squash than the ones used for jack-o'-lanterns. China and India combined account for half of the world's production of pumpkins.",
+
+        "The radish (Raphanus raphanistrum subsp. sativus) is an edible root vegetable of the family Brassicaceae that was domesticated in Asia prior to Roman times.Radishes are grown and consumed throughout the world, being mostly eaten raw as a crunchy salad vegetable with a pungent flavor. There are numerous varieties, varying in size, flavor, color, and length of time they take to mature. Radishes owe their sharp flavor to the various chemical compounds produced by the plants, including glucosinolate, myrosinase, and isothiocyanate. They are sometimes grown as companion plants and suffer from few pests and diseases. They germinate quickly and grow rapidly, common smaller varieties being ready for consumption within a month, while larger daikon varieties take several months. Being easy to grow and quick to harvest, radishes are often planted by novice gardeners. Another use of radish is as a cover or catch crop in winter, or as a forage crop. Some radishes are grown for their seeds; others, such as daikon, may be grown for oil production. Others are used for sprouting.\n",
+
+        "The radish (Raphanus raphanistrum subsp. sativus) is an edible root vegetable of the family Brassicaceae that was domesticated in Asia prior to Roman times.Radishes are grown and consumed throughout the world, being mostly eaten raw as a crunchy salad vegetable with a pungent flavor. There are numerous varieties, varying in size, flavor, color, and length of time they take to mature. Radishes owe their sharp flavor to the various chemical compounds produced by the plants, including glucosinolate, myrosinase, and isothiocyanate. They are sometimes grown as companion plants and suffer from few pests and diseases. They germinate quickly and grow rapidly, common smaller varieties being ready for consumption within a month, while larger daikon varieties take several months. Being easy to grow and quick to harvest, radishes are often planted by novice gardeners. Another use of radish is as a cover or catch crop in winter, or as a forage crop. Some radishes are grown for their seeds; others, such as daikon, may be grown for oil production. Others are used for sprouting.\n",
+
+        "Corn, also known as maize (North American and Australian English), is a cereal grain that was first domesticated by indigenous peoples in southern Mexico about 10,000 years ago. The leafy stalks produce pollen and ovulaferous inflorescences which are separate inflorescences called ears which produce the kernel or seeds, which are fruits. Corn has become a staple food in many parts of the world, with total maize production exceeding that of wheat or rice . Apart from being consumed directly by humans (often in the form of mass), corn is also used for corn ethanol, animal feed and other corn products, such as corn starch and corn syrup. The six main types of corn are crooked corn , rock corn , peas corn , popcorn , flour corn , and sweet corn . The sugar-rich variety called sweet corn is usually grown for human consumption as a kernel, while the field corn variety is used for animal feed, various human food uses based on corn (including milling into corn flour or masa, pressing into corn oil, and fermentation and distillation into a beverage. alcoholic beverages such as bourbon whiskey), and as a chemical feedstock. Corn is also used in the manufacture of ethanol and other biofuels.",
+
+        "The onion (Allium cepa L., from Latin cepa \"onion\"), also known as the bulb onion or common onion, is a vegetable that is the most widely cultivated species of the genus Allium. The shallot is a botanical variety of the onion. Until 2010, the shallot was classified as a separate species. Its close relatives include the garlic, scallion, leek, chive, and Chinese onion. This genus also contains several other species variously referred to as onions and cultivated for food, such as the Japanese bunching onion (Allium fistulosum), the tree onion (A. ×proliferum), and the Canada onion (Allium canadense). The name \"wild onion\" is applied to a number of Allium species, but A. cepa is exclusively known from cultivation. Its ancestral wild original form is not known, although escapes from cultivation have become established in some regions.[6] The onion is most frequently a biennial or a perennial plant, but is usually treated as an annual and harvested in its first growing season."
+
+
+        )
+
+}
